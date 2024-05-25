3f2f7416a55052b4fcae2016cb2b98f0c9e5c4af
dn46 <dn46@protonmail.ch>
2024-05-25 11:36:47 +0100

    cd added (no empty path yet)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -32,6 +33,19 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n") // removing the newline character at the end of the input
 
 	args := strings.Split(input, " ") // split the input to separate the command and the arguments (ls -l)
+
+	switch args[0] {
+	case "cd":
+		// gotta make cd to home dir with empty path
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+
+		// change directory and return the error
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
 
 	cmd := exec.Command(args[0], args[1:]...) // prepare the command to execute
 
